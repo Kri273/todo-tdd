bc8778d1b38340ef2cd49be2a9d10f51f6b23b76
Kri273 <kristelle.tasane@voco.ee>
2025-04-28 12:59:06 +0300

    Created TodoController.getTodoById unit test and integration tests

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,6 +3,7 @@ const TodoController = require("../controllers/todo.controller");
 const router = express.Router();
 
 router.post("/", TodoController.createTodo);
-router.get("/", TodoController.getTodos)
+router.get("/", TodoController.getTodos);
+router.get("/:todoId", TodoController.getTodoById);
 
 module.exports = router;
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,6 +4,8 @@ const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
+let firstTodo;
+
 describe(endpointUrl, () => {
   it("POST " + endpointUrl, async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
@@ -21,13 +23,28 @@ describe(endpointUrl, () => {
       expect(response.body).toStrictEqual({
         message: "Todo validation failed: done: Path `done` is required.",
       });
-    });
-    test("GET " + endpointUrl, async () => {
-      const response = await request(app).get(endpointUrl)
-      expect(response.statusCode).toBe(200)
-      expect(Array.isArray(response.body)).toBeTruthy()
-      expect(response.body[0].title).toBeDefined()
-      expect(response.body[0].done).toBeDefined()
-    }); 
-  
+    }
+  );
+  test("GET " + endpointUrl, async () => {
+    const response = await request(app).get(endpointUrl);
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
+  });
+
+  test("GET by Id " + endpointUrl + "todoId:", async () => {
+    const response = await request(app)
+    .get(endpointUrl + firstTodo._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+
+  test("GET todoby id doesn't exist " + endpointUrl + "todoId:", async () => {
+    const response = await request(app)
+    .get(endpointUrl + "650f3c3d0ac28f9d6458a3c3");
+    expect(response.statusCode).toBe(404);
+  });
 });
